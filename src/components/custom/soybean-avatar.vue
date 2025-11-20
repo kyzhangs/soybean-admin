9fee359a107c8226ecf2419323a9c45daa18ad03
kyzhangs <kyzhangs@foxmail.com>
2025-11-20 23:00:37 +0800

    optimize(components): optimize avatar component

--- src/components/custom/soybean-avatar.vue
+++ src/components/custom/soybean-avatar.vue
@@ -1,12 +1,25 @@
 <script setup lang="ts">
+import { computed } from 'vue';
+import defaultAvatar from '@/assets/imgs/soybean.jpg';
+
+interface Props {
+  url?: string;
+}
+
+const props = defineProps<Props>();
+
 defineOptions({
   name: 'SoybeanAvatar'
+});
+
+const avatarUrl = computed(() => {
+  return props.url || defaultAvatar;
 });
 </script>
 
 <template>
-  <div class="size-72px overflow-hidden rd-1/2">
-    <img src="@/assets/imgs/soybean.jpg" class="size-full" />
+  <div class="size-72px overflow-hidden border rd-1/2">
+    <img :src="avatarUrl" class="size-full" />
   </div>
 </template>
 
--- src/typings/api/auth.d.ts
+++ src/typings/api/auth.d.ts
@@ -13,6 +13,7 @@ declare namespace Api {
     interface UserInfo {
       userId: string;
       userName: string;
+      avatar: string;
       roles: string[];
       buttons: string[];
     }
