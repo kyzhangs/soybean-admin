8b82b2f67375351d60615017bf2a54ea4a242728
kyzhangs <kyzhangs@foxmail.com>
2026-09-23 17:46:52 +0800

    test(projects): ceshi

--- src/views/about/index.vue
+++ src/views/about/index.vue
@@ -42,7 +42,7 @@
 <template>
   <div>
     <iframe
-      src="https://ai.show.wisedu.com/chatbot/mxn51fMQPAKpJzEQ"
+      src="https://ai.show.wisedu.com/chat-plus/mxn51fMQPAKpJzEQ"
       style="width: 100%; height: 100%; min-height: 700px"
       frameborder="0"
       allow="microphone"
